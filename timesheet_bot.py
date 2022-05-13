f81706e69ab87e1dcb7b6ddb395078b2f16c2b1f
Matthew Thornton <44626690+ThorntonMatthewD@users.noreply.github.com>
2022-05-12 20:32:49 -0400

    Add black formatter

--- timesheet_bot.py
+++ timesheet_bot.py
@@ -8,10 +8,10 @@ from discord_webhook.webhook import DiscordEmbed
 
 load_dotenv(find_dotenv(), verbose=True)
 
-DISCORD_WEBHOOK_URL = os.environ.get('webhook')
-ROLE_TO_PING = os.environ.get('ping_id')
+DISCORD_WEBHOOK_URL = os.environ.get("webhook")
+ROLE_TO_PING = os.environ.get("ping_id")
 
-#BAH Holidays for 2022
+# BAH Holidays for 2022
 HOLIDAYS = [
     "05/30/2022",
     "06/20/2022",
@@ -19,14 +19,14 @@ HOLIDAYS = [
     "09/05/2022",
     "10/10/2022",
     "11/24/2022",
-    "12/26/2022"
+    "12/26/2022",
 ]
 
-#Gets the last working day before the 15th and end of a month.
-#Has to account for weekends and holidays..
+# Gets the last working day before the 15th and end of a month.
+# Has to account for weekends and holidays..
 def check_if_sign_date():
     now = datetime.datetime.now()
-    
+
     if now.day > 15:
         days_in_month = calendar.monthrange(now.year, now.month)[1]
         target_date = datetime.datetime(now.year, now.month, days_in_month)
@@ -44,11 +44,11 @@ def get_next_sign_date(target_date):
     days_offset = 0
 
     while not_found:
-        target_date = target_date + datetime.timedelta(days = days_offset)
-    
-        #check if weekday
+        target_date = target_date + datetime.timedelta(days=days_offset)
+
+        # check if weekday
         if target_date.isoweekday() < 6:
-            #check if holiday
+            # check if holiday
             if check_if_holiday(target_date):
                 days_offset -= 1
             else:
@@ -70,22 +70,31 @@ def check_if_holiday(date):
 
 
 def send_discord_webhook():
-    embed = DiscordEmbed(title="It's TIME to Submit Your Timesheet!", color="01807e",
-                             description=f"Don't forget to sign your timesheet in Deltek today!")
+    embed = DiscordEmbed(
+        title="It's TIME to Submit Your Timesheet!",
+        color="01807e",
+        description=f"Don't forget to sign your timesheet in Deltek today!",
+    )
     embed.set_timestamp()
-    embed.add_embed_field(name="Log in now!", value="https://time.bah.com", inline=False)
-    embed.set_thumbnail(url="https://i.kym-cdn.com/photos/images/original/001/879/961/056.gif")
+    embed.add_embed_field(
+        name="Log in now!", value="https://time.bah.com", inline=False
+    )
+    embed.set_thumbnail(
+        url="https://i.kym-cdn.com/photos/images/original/001/879/961/056.gif"
+    )
     webhook = DiscordWebhook(url=DISCORD_WEBHOOK_URL, rate_limit_retry=True)
     webhook.add_embed(embed=embed)
     webhook.execute()
 
-    webhook = DiscordWebhook(url=DISCORD_WEBHOOK_URL, rate_limit_retry=True, content=ROLE_TO_PING)
+    webhook = DiscordWebhook(
+        url=DISCORD_WEBHOOK_URL, rate_limit_retry=True, content=ROLE_TO_PING
+    )
     webhook.execute()
 
 
 if __name__ == "__main__":
     if check_if_sign_date():
         print("It is time to submit your timesheet.")
-        send_discord_webhook()
+        # send_discord_webhook()
     else:
         print("It isn't time for submitting your timesheet.")
